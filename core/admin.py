c5f12878dba51d24972ebe40f76643d15bf3ee40
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-06-28 04:50:32 -0300

    Criado os eventos da agenda e os filtros de busca

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from core.models import Evento
+
 
 # Register your models here.
+
+
+class EventoAdmin(admin.ModelAdmin):
+    # Para definir quais colunas mostrar na tabela
+    list_display = ('titulo', 'data_evento', 'data_criacao', 'usuario')
+    # Adicionando filtros para busca na página do Django-Admin
+    list_filter = ('titulo', 'usuario', 'data_evento')
+
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Evento (models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) # Para usar os usuários do Django
+
+    class Meta:
+        db_table = 'evento'
+
+    # Para exibir o nome do campo no Django-Admin
+    def __str__(self):
+        return self.titulo
