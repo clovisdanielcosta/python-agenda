29709c9896c99a28ea1f8741ed93016d3856e4a6
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-06-29 00:29:36 -0300

    Ajustes em Views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def consulta_evento(request, titulo_evento):
+
+    data_ev = HttpResponse.Evento.get(titulo=titulo_evento)
+
+    return HttpResponse('O Evento {} será em' .format(data_ev))
+
+
+
