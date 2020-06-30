3b2e5722876aacdc90b3c6402dd288451d5fa0a0
clovisdanielcosta <58163677+clovisdanielcosta@users.noreply.github.com>
2020-06-30 02:15:23 -0300

    Criado telas de login e logout

--- core/views.py
+++ core/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from  django.contrib import messages
 
 # Create your views here.
+
+def login_user(request):
+        return render(request, 'login.html')
+
+def logout_user(request):
+        logout(request)
+        return redirect('/')
+
+def submit_login(request):
+        if request.POST:
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                usuario = authenticate(username=username, password=password)
+                if usuario is not None:
+                        login(request, usuario)
+                        return redirect('/')
+                else:
+                        messages.error(request, 'Usuário ou Senha inválidos')
+        return  redirect('/')
 
 # caso seja precisa usar uma view para o index importe o 'redirect' tbem de 'shortcurts'
 # def index(request):
 #      return redirect('/agenda/')
-
+@login_required(login_url='/login/') # Precisa colocar a barra no início pra não concatenar
 def lista_eventos(request):
         # evento = Evento.objects.get(id=1) #para consulta pelo id
         # Pelo usuário
-        # usuario = request.user
-        # evento = Evento.objects.filter(usuario=usuario)
-        evento = Evento.objects.all() #todos
+        #evento = Evento.objects.all() #todos
+        usuario = request.user
+        evento = Evento.objects.filter(usuario=usuario)
         dados = {'eventos':evento}
         return render(request, 'agenda.html', dados)
 
