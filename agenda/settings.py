b9e8eca091c0b86f03410d8646fbdf3b4eac4037
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-07-13 12:49:31 -0300

    Configurações do django-heroku para deploy

--- agenda/settings.py
+++ agenda/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,6 +126,9 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
 
