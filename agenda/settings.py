a60efaaf1920c1788ab6b2cab0ea2256e2cfde25
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-07-13 18:34:08 -0300

    Ajuste nas configurações de Static files em settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -135,7 +135,17 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 Activate Django-Heroku.
 django_heroku.settings(locals())
