bc112c3c6b10e87623b4bda6630b926e3d778618
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-07-13 19:10:12 -0300

    Ajuste nas configurações de Static files em settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -141,7 +141,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
@@ -150,7 +149,6 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
-
 
 Activate Django-Heroku
 django_heroku.settings(locals())
