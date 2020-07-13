cd11b45a26ef90db825cf480d935b58355d01b08
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2020-07-13 18:52:16 -0300

    Ajuste nas configurações de Static files em settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u8ygg+9s7@ms4!zb3#g$=b9iq%$0l4i!oj)-!zj4ma%&us_g$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
